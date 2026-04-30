19f62650b5492d2cdaded7157a5d1e10ef2b40ba
Navnath <navnyajade@gmail.com>
2026-04-30 11:04:43 +0530

    string

---
+++ introstr.py
@@ -0,0 +1,15 @@
+name = "Harry"
+
+nameshort = name[0:4] 
+
+print(nameshort)
+
+character1 = name[2]
+print(character1)
+
+print(name[-4:-1])
+print(name[1:4])
+
+print(name[:4])# is same as print(name[0:4])
+print(name[1:])# is same as print(name[1:5])
+print(name[1:5])
